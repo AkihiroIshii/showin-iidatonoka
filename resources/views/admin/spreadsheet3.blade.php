c25df4bdd03d5b2b6613e45c5532108ec0e85f8b
AkihiroIshii <ishii.akihiro@gmail.com>
2025-02-01 13:00:25 +0900

    管理者画面でも集計表３に全問を表示。

--- resources/views/admin/spreadsheet3.blade.php
+++ resources/views/admin/spreadsheet3.blade.php
@@ -8,6 +8,8 @@
     <div class="mx-auto px-6">
         @php
             $maxNos = ['国語' => 6, '数学' => 5, '社会' => 4, '理科' => 5, '英語' => 5];
+            $nos = ['1','2','3','4','5','全問'];
+            $nosDisp = ['問１','問２','問３','問４','問５','全問'];
             $questionsSet = $questionsSet->groupBy('subject')->map(fn($group) => $group->groupBy('year'));
         @endphp
         <div>
@@ -21,8 +23,8 @@
                     <thead>
                         <tr>
                             <th class="border border-gray-400 px-4 py-2">年度</th>
-                            @for($i = 1; $i <= 5; $i++)
-                                <th class="border border-gray-400 px-4 py-2">問 {{ $i }}</th>
+                            @for($i = 0; $i <= 5; $i++)
+                                <th class="border border-gray-400 px-4 py-2">{{ $nosDisp[$i] }}</th>
                             @endfor
                         </tr>
                     </thead>
@@ -31,9 +33,9 @@
                             <tr>
                                 <td class="border border-gray-400 px-4 py-2">{{ $year }}</td>
                                 
-                                @for($i = 1; $i <= 5; $i++)
+                                @for($i = 0; $i <= 5; $i++)
                                     @php
-                                        $question = $questionsByYear->firstWhere('no', $i);
+                                        $question = $questionsByYear->firstWhere('no', $nos[$i]);
                                     @endphp
                                     <td class="border border-gray-400 px-4 py-2">
                                         @if($question)
