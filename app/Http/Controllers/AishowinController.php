a5195701a75f308232d4f37e304ad487e0631ad8
AkihiroIshii <ishii.akihiro@gmail.com>
2025-02-18 16:48:06 +0900

    目的別対策のデータ抽出を、複数のタグに対応するよう修正。

--- app/Http/Controllers/AishowinController.php
+++ app/Http/Controllers/AishowinController.php
@@ -8,16 +8,16 @@ use App\Models\Aishowin;
 class AishowinController extends Controller
 {
     public function index(){
-        $trainUnits = Aishowin::where('tag', '=', '計算力を鍛える')
+        $trainUnits = Aishowin::where('tag', 'like', '%計算力を鍛える%')
             ->get();
 
-        $scienceUnits = Aishowin::where('tag', '=', '中学理科')
+        $scienceUnits = Aishowin::where('tag', 'like', '%中学理科%')
             ->get();
 
-            $societyUnits = Aishowin::where('tag', '=', '中学社会')
+            $societyUnits = Aishowin::where('tag', 'like', '%中学社会%')
             ->get();
 
-            $appliedUnits = Aishowin::where('tag', '=', '応用問題')
+            $appliedUnits = Aishowin::where('tag', 'like', '%応用問題%')
             ->get();
 
         return view('aishowin.index', compact('trainUnits','scienceUnits','societyUnits','appliedUnits'));
