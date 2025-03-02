2340246e85552524f5ce7cf3eabd6be60c04ffa2
AkihiroIshii <ishii.akihiro@gmail.com>
2025-03-02 15:11:29 +0900

    複数生徒をもつ保護者でもすべての対象イベントが取得されるよう修正。

--- app/Traits/EventTrait.php
+++ app/Traits/EventTrait.php
@@ -20,6 +20,7 @@ trait EventTrait
         $user_ids = Arr::wrap(Session::get('target_students'));
         $school_ids = User::whereIn('id', $user_ids)->pluck('school_id')->toArray();
         $school_ids = array_merge($school_ids, [901, 902]); //松陰塾と外部のIDを追加。
+        $user_grades = User::whereIn('id', $user_ids)->pluck('grade')->toArray();
 
         // 直近２ヵ月のイベントを取得
         $events = Event::whereBetween('events.date_from', [$startDate, $endDate])
@@ -40,9 +41,21 @@ trait EventTrait
 
         // 生徒に関連する学校のイベントに絞り込み。
         if(Auth::user()->role !== "admin") {
-            $events = $events->whereIn('school_id', $school_ids);
+            $events = $events->whereIn('school_id', $school_ids)
+                //生徒の学年が対象のイベントに絞り込み。
+                ->where(function($query) use ($user_grades) {
+                    $query->orWhereNull('grade')
+                        ->orWhere('grade', "")
+                        ->orWhere(function ($query2) use($user_grades) {
+                            foreach ($user_grades as $user_grade) {
+                                $query2->orWhere('grade', 'LIKE', "%{$user_grade}%");
+                            }
+                        });
+                });
         }
-        $events = $events->get();
+        $events = $events
+            ->orderBy('events.date_from', 'asc')
+            ->get();
         return $events;
     }
 }
