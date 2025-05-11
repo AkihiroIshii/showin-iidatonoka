c8f1bb1e580897dd60674a536fd49de7f1cc914c
AkihiroIshii <ishii.akihiro@gmail.com>
2025-05-11 18:05:37 +0900

    ユーザ一覧に、挑戦中の目標を表示するように修正。

--- app/Traits/UserTrait.php
+++ app/Traits/UserTrait.php
@@ -3,16 +3,35 @@
 namespace App\Traits;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Models\User;
+
+// use Carbon\Carbon;
 
 trait UserTrait
 {
     public function getUsers() {
+        
+        // 未達成の目標のうち、もっとも日付が前の１件をユーザごとに取得
+        $subQuery = DB::table('usualtargets as ut1')
+        ->select('ut1.*')
+        ->whereNull('ut1.achieve_flg')
+        ->whereRaw('ut1.id = (
+            SELECT ut2.id FROM usualtargets as ut2
+            WHERE ut2.user_id = ut1.user_id AND ut2.achieve_flg IS NULL
+            ORDER BY ut2.due_date ASC
+            LIMIT 1
+        )');
+
+        // ユーザ一覧を取得
         $users = User::where('grade', '!=', '保護者')
             // ->whereNull('expiration_date')
             ->leftJoin('schools', function($join) {
                 $join->on('users.school_id', '=', 'schools.id');
+            })
+            ->leftJoinSub($subQuery, 'ut', function($join) {
+                $join->on('users.id', '=', 'ut.user_id');
             })
             ->selectRaw('
                 users.id,
@@ -20,7 +39,9 @@ trait UserTrait
                 schools.name as school_name,
                 users.grade,
                 users.plan,
-                users.expiration_date
+                users.expiration_date,
+                ut.content,
+                ut.due_date
             ')
             ->orderBy('users.expiration_date','asc')
             ->orderBy('users.grade','desc')
--- resources/views/admin/students.blade.php
+++ resources/views/admin/students.blade.php
@@ -35,6 +35,7 @@
                         <th style="position:sticky;top:0;background-color:white;" class="border border-slate-300 px-4">生徒名</td>
                         <th style="position:sticky;top:0;background-color:white;" class="border border-slate-300 px-4">学年</td>
                         <th style="position:sticky;top:0;background-color:white;" class="border border-slate-300 px-4">通塾頻度</td>
+                        <th style="position:sticky;top:0;background-color:white;" class="border border-slate-300 px-4">挑戦中の目標</td>
                     </tr>
                     @foreach($users as $user)
                         @php
@@ -57,6 +58,11 @@
                             <td class="border border-slate-300 px-4">{{$user->user_name}}</td>
                             <td class="border border-slate-300 px-4">{{$user->grade}}</td>
                             <td class="border border-slate-300 px-4">{{$user->plan}}</td>
+                            <td class="border border-slate-300 px-4">
+                                @if(isset($user->content))
+                                    {{$user->due_date}}：{{$user->content}}
+                                @endif
+                            </td>
                         </tr>
                     @endforeach
                 </table>
