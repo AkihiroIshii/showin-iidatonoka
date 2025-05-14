5b642c1ec62b10a96d6ec2f6894607ff41ecc5e8
AkihiroIshii <ishii.akihiro@gmail.com>
2025-05-14 14:26:35 +0900

    試験の問題や解説を別タブで開くように修正。

--- resources/views/exam/show.blade.php
+++ resources/views/exam/show.blade.php
@@ -23,41 +23,41 @@
                 </tr>
                 <!-- 問題 -->
                 <tr>
-                    <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_jap_q.pdf']) }}">問題</a></td>
-                    <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_soc_q.pdf']) }}">問題</a></td>
-                    <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_math_q.pdf']) }}">問題</a></td>
-                    <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_sci_q.pdf']) }}">問題</a></td>
-                    <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_eng_q.pdf']) }}">問題</a></td>
+                    <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" target="_blank" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_jap_q.pdf']) }}">問題</a></td>
+                    <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" target="_blank" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_soc_q.pdf']) }}">問題</a></td>
+                    <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" target="_blank" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_math_q.pdf']) }}">問題</a></td>
+                    <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" target="_blank" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_sci_q.pdf']) }}">問題</a></td>
+                    <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" target="_blank" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_eng_q.pdf']) }}">問題</a></td>
                 </tr>
                 <!-- 解答 -->
                 @if($folder == "schoolexam")
                     <tr>
-                        <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_jap_a.pdf']) }}">解答</a></td>
-                        <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_soc_a.pdf']) }}">解答</a></td>
-                        <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_math_a.pdf']) }}">解答</a></td>
-                        <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_sci_a.pdf']) }}">解答</a></td>
-                        <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_eng_a.pdf']) }}">解答</a></td>
+                        <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" target="_blank" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_jap_a.pdf']) }}">解答</a></td>
+                        <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" target="_blank" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_soc_a.pdf']) }}">解答</a></td>
+                        <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" target="_blank" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_math_a.pdf']) }}">解答</a></td>
+                        <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" target="_blank" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_sci_a.pdf']) }}">解答</a></td>
+                        <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" target="_blank" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_eng_a.pdf']) }}">解答</a></td>
                     </tr>
                 @endif
                 <!-- 解説 -->
                 <tr>
-                    <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_jap_e.pdf']) }}">解説</a></td>
-                    <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_soc_e.pdf']) }}">解説</a></td>
-                    <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_math_e.pdf']) }}">解説</a></td>
-                    <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_sci_e.pdf']) }}">解説</a></td>
-                    <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_eng_e.pdf']) }}">解説</a></td>
+                    <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" target="_blank" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_jap_e.pdf']) }}">解説</a></td>
+                    <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" target="_blank" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_soc_e.pdf']) }}">解説</a></td>
+                    <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" target="_blank" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_math_e.pdf']) }}">解説</a></td>
+                    <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" target="_blank" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_sci_e.pdf']) }}">解説</a></td>
+                    <td class="border border-slate-300 px-4"><a class="font-semibold text-blue-600" target="_blank" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_eng_e.pdf']) }}">解説</a></td>
                 </tr>
             </table>
             
             <!-- 学校の試験の場合 -->
             @if($folder == "schoolexam")
-                <p class="text-center">(※)解説はない場合もあります。</p>
+                <p class="text-center">(※)別タブで開きます。解説はない場合もあります。</p>
             <!-- 模試の場合 -->
             @elseif($folder == "moshi")
                 <div class="mt-6 text-center">
                     <ul>
-                        <li><a class="font-semibold text-blue-600" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_answersheet.pdf']) }}">解答用紙</a></li>
-                        <li><a class="font-semibold text-blue-600" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_kaito_kaisetsu.pdf']) }}">解答と解説</a></li>
+                        <li><a class="font-semibold text-blue-600" target="_blank" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_answersheet.pdf']) }}">解答用紙</a></li>
+                        <li><a class="font-semibold text-blue-600" target="_blank" href="{{ route('secure.file', ['folder' => $folder, 'filename' => 'exam_' . $exam->id . '_kaito_kaisetsu.pdf']) }}">解答と解説</a></li>
                     </ul>
                 </div>
             @endif
